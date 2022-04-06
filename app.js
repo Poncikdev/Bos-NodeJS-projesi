bc71e228edf61c419d82112b2bb9bb5b9662891a
Poncikdev <103049241+Poncikdev@users.noreply.github.com>
2022-04-06 17:30:57 +0300

    Update app.js

--- app.js
+++ app.js
@@ -19,9 +19,9 @@ console.log(1.84393893849384939389 * (10 ** 308))
 /**
  * console.log("Hello World!") kodu Terminale mesaj yazdırır.
  * 
- * Örneği test.js dosyasında bulunmaktadır. ("Hello World!") kısmını istediğiniz gibi değiştirebilirsiniz.
- * 
  * 1. Satıra console.log("Terminale yazdıracağın mesaj") gibi...
+ * 
+ * Örneği test.js dosyasında bulunmaktadır. ("Hello World!") kısmını istediğiniz gibi değiştirebilirsiniz.
  * 
  * 
  */
