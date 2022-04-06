359ae6a91cac5ea1370fe9b2115ac6fe1234e76b
Poncikdev <103049241+Poncikdev@users.noreply.github.com>
2022-04-06 17:28:51 +0300

    Create [poncik]app.js

---
+++ [poncik]app.js
@@ -0,0 +1,27 @@
+// %70 NodeJS yazılım dili bilmiyorsan çok fazla kurcalama.
+
+var test
+
+// Number
+test = 3162693131
+
+// String
+test = "test"
+
+//boolean
+test = false
+
+// BigInt
+test = 2n
+
+console.log(1.84393893849384939389 * (10 ** 308))
+
+/**
+ * console.log("Hello World!") kodu Terminale mesaj yazdırır.
+ * 
+ * Örneği test.js dosyasında bulunmaktadır. ("Hello World!") kısmını istediğiniz gibi değiştirebilirsiniz.
+ * 
+ * 1. Satıra console.log("Terminale yazdıracağın mesaj") gibi...
+ * 
+ * 
+ */
